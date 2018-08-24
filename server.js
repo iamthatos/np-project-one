5a8c96dd298dec9068e8f75f2ad01052de84f1dd
Thato Seboko <thato.seboko@gmail.com>
2018-08-24 15:39:43 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} : ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+
+    console.log(`${now} : ${req.method} : ${req.url}`);
+
+
+    next();
+});
+
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        welcomeMessage: 'Welcome to your home page.',
+        pageTitle: 'Home Page',
+        currntYear: new Date().getFullYear()
+    })
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        pageTitle: 'About Page Dynamic',
+        currntYear: new Date().getFullYear()
+    })
+});
+
+app.get('/bad', (req, res) =>{
+    res.send({
+        status: 'ERROR',
+        code: '404',
+        message: 'Bad URI, api cannot be reached'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
